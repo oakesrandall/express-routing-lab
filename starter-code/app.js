dceea846b2a0065a76ef7c6a086a4bf407353786
David_Randall <oakesrandall@gmail.com>
2017-08-04 14:21:37 -0600

    fixed error

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const app     = express();
-const port    = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 const candyRouter = require('./candyRouter.js');
-const parseBody = require('body-parser');
 
+app.use(bodyParser.json());
 
-
-
-app.use(router.parseBody("candies"));
-
-
-//How do we redirect the /candies path
-//through our candyRouter?
-//Hint: you need app.use
 app.use('/candies', candyRouter);
 
-app.listen(port);
+app.listen(port, function() {
+    console.log('Server started on port: ', port);
+});
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -11,19 +11,23 @@ var candies = [
 ];
 
 router.get('/', function(req, res) {
+  console.log('hit get route on /');
   res.send(candies);
 });
 
 router.get('/:id', function(req, res) {
+  console.log('hit get route on /:id ' + req.params.id);
   res.send(candies[req.params.id -1]);
 });
 
 router.post('/', function(req, res){
+    console.log('hit post route on /');
     var newCandy = {'_id': candies.length += 1, 'name': req.body.name, 'color': req.body.color};
     res.send(newCandy);  
 });
 
 router.put('/:id', function(req, res) {
+    console.log('hit put route on /: ' + req.params.id);
     var updateCandy = candies[req.params.id-1];
     updateCandy.name = res.body.name;
     updateCandy.color = res.body.color;
@@ -31,6 +35,7 @@ router.put('/:id', function(req, res) {
 });
 
 router.delete('/:id', function(req, res) {
+    console.log('hit delete route on /:id ' + req.params.id);
     candies.splice(req.params.id-1, 1);
     res.send(req.body);
 });
